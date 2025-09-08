efa8b4dcd25c28b9f443ef4b88fdd662d55a6043
Hamza Mirza <100884757+ham20000@users.noreply.github.com>
2025-09-07 23:13:34 -0500

    added linked list implementation

--- Starter.cpp
+++ Starter.cpp
@@ -1,6 +1,71 @@
 #include <iostream>
+
+class Node {
+	public:
+		int data;
+		Node *next;
+	
+	Node(){
+		data = 0;
+		next = NULL;
+	}
+
+	Node (int data) {
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+class Linkedlist {
+	Node *head;
+	public:
+		Linkedlist() {
+			head = NULL;
+		}
+		
+		void insertAtHead(int data) {
+			Node *newNode = new Node(data);
+			
+			if (head == NULL) {
+				head = newNode;
+				return;
+			}
+
+			newNode->next = this->head;
+			this->head = newNode;
+		}
+
+		void print() {
+			Node *temp = head;
+			
+			if(head == NULL) {
+				std::cout << "List empty" << '\n';
+				return;
+			}
+
+			while (temp != NULL) {
+				std::cout << temp->data << " ";
+				temp = temp->next;
+			}
+		}
+
+};
+
 int main() {
 
+	
+	Linkedlist list;
+
+
+	list.insertAtHead(20);
+	list.insertAtHead(21);
+	list.insertAtHead(22);
+	list.insertAtHead(23);
+
+	std::cout << "Elements of the list are: ";
+	list.print();
+	std::cout << '\n';
+	
 	int x = 5;
 	int y = 6;
 	int sum = x+y;
